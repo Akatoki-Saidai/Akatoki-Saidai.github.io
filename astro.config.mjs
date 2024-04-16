932d3273a2734d3fc59bcec51eef8769853a1bf8
takashin9 <takashi64256@gmail.com>
2024-04-16 19:55:59 +0900

    baseの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://akatoki-saidai.github.io'
+  site: 'https://akatoki-saidai.github.io',
+  base: '/'
 });
